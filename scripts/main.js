78abd5bf638665e7c7486b3b9d16b225fa86b5b8
José Miguel Sarasola <Josemsar92@gmail.com>
2015-12-27 18:48:17 +0100

    update amountScrolled for Phone

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 });
 
 $(document).scroll(function() {
-    var amountScrolled = 500;
+    var amountScrolled = 100;
     
 	if ( $(window).scrollTop() > amountScrolled ) {
 		$('a.back-to-top').fadeIn('slow');
