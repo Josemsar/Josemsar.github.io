223c71b63948b1b38cde8f86a473516297de116f
José Miguel Sarasola <Josemsar92@gmail.com>
2015-12-27 18:28:01 +0100

    Basic JavaScript and "Go to Top" button added

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+// create the back to top button
+$(document).ready(function(){
+   
+    function goToTop() {
+	   $('html').animate({
+		scrollTop: 0}, 'slow');
+    };
+
+    function prependBackToTop(){
+        $('body').prepend('<a onclick="goToTop()" href="#" class="back-to-top">Back to Top</a>');
+    }
+    
+    prependBackToTop();
+});
+
+$(document).scroll(function() {
+    var amountScrolled = 10;
+    
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
